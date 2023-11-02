1fa76171cd96051dba12e6086f3a5e3b52365740
Prachi <sriprachi2709@gmail.com>
2023-11-02 13:28:46 +0530

    first commit

---
+++ src/components/MessageDisplay.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+function MessageDisplay(){
+    return (
+        <div className="message-display">
+            <p id="icon">X</p>
+            <p>user</p>
+            <p>message</p>
+        </div>
+    )
+}
+
+export default MessageDisplay;
---
+++ src/components/MessagesDisplay.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import MessageDisplay from "./MessageDisplay";
+
+function MessagesDisplay(){
+    return (
+        <div className="messages-display">
+            <MessageDisplay />
+        </div>
+    )
+}
+
+export default MessagesDisplay;
